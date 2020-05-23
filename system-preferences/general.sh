d140332ee151c6b97d9a01f4d8817388a22a301f
Will B Chang <will.b.chang@gmail.com>
2020-05-23 22:33:41 +0800

    feat: General - Set appearance to auto

---
+++ system-preferences/general.sh
@@ -0,0 +1,4 @@
+# Set appearance to auto
+# Required login and logout computer.
+# https://apple.stackexchange.com/a/391814/306411
+defaults write ~/Library/Preferences/.GlobalPreferences AppleInterfaceStyleSwitchesAutomatically -bool true
