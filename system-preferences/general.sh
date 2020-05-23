558920f0184dc2b392db1564ee7bb4611ee530d9
Will B Chang <will.b.chang@gmail.com>
2020-05-23 22:34:46 +0800

    feat: General - Set accent color to pink

--- system-preferences/general.sh
+++ system-preferences/general.sh
@@ -2,3 +2,18 @@
 # Required login and logout computer.
 # https://apple.stackexchange.com/a/391814/306411
 defaults write ~/Library/Preferences/.GlobalPreferences AppleInterfaceStyleSwitchesAutomatically -bool true
+
+# Set accent color to Pink
+# Accent Color
+# Red           : 0
+# Orange        : 1
+# Yellow        : 2
+# Green         : 3
+# Blue          : 4
+# Purple        : 5
+# Pink          : 6
+# Logan         : 7
+# Paris Daisy   : 8
+# Coral         : 9
+# Silver        : 10
+defaults write -g AppleAccentColor 6
