15041de4bd198b1f8d262a949a5c7b2e4930d88b
Will B Chang <will.b.chang@gmail.com>
2020-08-04 15:04:07 +0800

    Set Samantha to the default speaker.

--- system-preferences/accessibility.sh
+++ system-preferences/accessibility.sh
@@ -1,2 +1,7 @@
 # Enable Option + Esc to speak selected text.
 defaults write com.apple.speech.synthesis.general.prefs SpokenUIUseSpeakingHotKeyFlag -bool true
+
+# Set Samantha to the default speaker.
+defaults write com.apple.speech.voice.prefs SelectedVoiceCreator -int 1886745202
+defaults write com.apple.speech.voice.prefs SelectedVoiceID -int 184844483
+defaults write com.apple.speech.voice.prefs SelectedVoiceName -string "Samantha"
