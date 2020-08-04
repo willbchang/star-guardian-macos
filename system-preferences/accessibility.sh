02fa464cb2d3e926adbb890653f6c3e6394818e8
Will B Chang <will.b.chang@gmail.com>
2020-08-04 23:34:01 +0800

    system preferences(accessibility): Enable three finger drag

--- system-preferences/accessibility.sh
+++ system-preferences/accessibility.sh
@@ -5,3 +5,9 @@ defaults write com.apple.speech.synthesis.general.prefs SpokenUIUseSpeakingHotKe
 defaults write com.apple.speech.voice.prefs SelectedVoiceCreator -int 1886745202
 defaults write com.apple.speech.voice.prefs SelectedVoiceID -int 184844483
 defaults write com.apple.speech.voice.prefs SelectedVoiceName -string "Samantha"
+
+# Enable three finger drag
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
+defaults write com.apple.AppleMultitouchTrackpad TrackpadFourFingerHorizSwipeGesture -int 2
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerVertSwipeGesture -int 0
+defaults write com.apple.AppleMultitouchTrackpad DragLock -bool false
