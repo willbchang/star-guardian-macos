c9187ad5be862b04cde32cd829f96d70846db126
Will B Chang <will.b.chang@gmail.com>
2020-08-06 09:37:15 +0800

    refactor: reorder keyboard.sh with categories

--- system-preferences/keyboard.sh
+++ system-preferences/keyboard.sh
@@ -1,22 +1,4 @@
-echo 'Disable automatic capitalization'
-defaults write -g NSAutomaticCapitalizationEnabled -bool false
-
-echo 'Disable smart dashes'
-defaults write -g NSAutomaticDashSubstitutionEnabled -bool false
-
-echo 'Disable automatic period substitution'
-defaults write -g NSAutomaticPeriodSubstitutionEnabled -bool false
-
-echo 'Disable smart quotes'
-defaults write -g NSAutomaticQuoteSubstitutionEnabled -bool false
-
-echo 'Disable auto-correct'
-defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
-
-echo 'Enable full keyboard access for all controls'
-# (e.g. enable Tab in modal dialogs)
-defaults write -g AppleKeyboardUIMode -int 3
-
+# Keyboard
 echo 'Disable press-and-hold for keys in favor of key repeat'
 defaults write -g ApplePressAndHoldEnabled -bool false
 
@@ -41,6 +23,27 @@ defaults write com.apple.controlstrip FullCustomized '(
 echo 'Expaned control strip by default'
 defaults write com.apple.touchbar.agent PresentationModeGlobal -string "fullControlStrip"
 
+# Text
+echo 'Disable automatic capitalization'
+defaults write -g NSAutomaticCapitalizationEnabled -bool false
+
+echo 'Disable smart dashes'
+defaults write -g NSAutomaticDashSubstitutionEnabled -bool false
+
+echo 'Disable automatic period substitution'
+defaults write -g NSAutomaticPeriodSubstitutionEnabled -bool false
+
+echo 'Disable smart quotes'
+defaults write -g NSAutomaticQuoteSubstitutionEnabled -bool false
+
+echo 'Disable auto-correct'
+defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
+
+# Shortcuts
+echo 'Enable full keyboard access for all controls'
+# (e.g. enable Tab in modal dialogs)
+defaults write -g AppleKeyboardUIMode -int 3
+
 echo 'Disable shortcuts to "Turn Dock Hiding On/Off"'
 /usr/libexec/PlistBuddy -c "set :AppleSymbolicHotKeys:52:enabled false" ~/Library/Preferences/com.apple.symbolichotkeys.plist
 
@@ -55,3 +58,5 @@ echo 'Disable shortcuts to "Show Spotlight Search"'
 
 echo 'Disable shortcuts to "Show Finder search window"'
 /usr/libexec/PlistBuddy -c "set :AppleSymbolicHotKeys:64:enabled false" ~/Library/Preferences/com.apple.symbolichotkeys.plist
+
+# Input Sources
