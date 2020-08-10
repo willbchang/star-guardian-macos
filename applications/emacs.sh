9b66551db1e8d21ac34d91ec3b829ff360bebabf
Will B Chang <will.b.chang@gmail.com>
2020-08-10 17:16:21 +0800

    applications(emacs): Install emacs-plus@27 and dependencies for doom emacs

---
+++ applications/emacs.sh
@@ -0,0 +1,7 @@
+echo 'Install emacs-plus27 and dependencies for doom emacs'
+brew install emacs-plus@27
+brew install ripgrep
+
+echo 'Install Doom Emacs'
+git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
+~/.emacs.d/bin/doom install
