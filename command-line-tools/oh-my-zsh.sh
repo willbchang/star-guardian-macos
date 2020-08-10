770a48f910d9a23a0dfde60873d572d39be3d647
Will B Chang <will.b.chang@gmail.com>
2020-08-10 14:28:23 +0800

    command-line-tools(oh-my-zsh): add plugin: Zsh Syntax Highlighting

--- command-line-tools/oh-my-zsh.sh
+++ command-line-tools/oh-my-zsh.sh
@@ -9,6 +9,9 @@ echo 'Install Oh-My-Zsh plugin: Zsh Completions'
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 autoload -U compinit && compinit
 
+echo 'Install Oh-My-Zsh plugin: Zsh Syntax Highlighting'
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 echo 'Reload Oh-My-Zsh configurations'
 source ~/.zshrc
 
