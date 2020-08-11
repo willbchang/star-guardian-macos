dd63e4d8ea0c52bfe363b4804324488f6dea64c9
Will B Chang <will.b.chang@gmail.com>
2020-08-11 17:19:40 +0800

    applications(alfred): add preferences

---
+++ applications/Alfred.alfredpreferences/workflows/user.workflow.4DAC27EB-C5F1-4FA5-9B9B-D59141ACA3FA/emacs.rb
@@ -0,0 +1,19 @@
+require 'shellwords'
+
+# Remove single quote around file path
+# Extrac '' are added when selecting from Alfred File Browser
+filepath = ARGV[0].gsub(/^'|'$/, '')
+
+if File.exist?(filepath)
+  # https://www.gnu.org/software/emacs/manual/html_node/emacs/emacsclient-Options.html
+  create_frame = File.directory?(filepath) ? '-c' : ''
+  cmd = "/usr/local/bin/emacsclient #{create_frame} -n #{filepath.shellescape}"
+  # If system cmd gets an error, it will run the code inside instead
+  # Usually emacs server doesn't start, or emacs isn't activate
+  unless system cmd
+    `open -a Emacs #{filepath.shellescape}`
+  end
+else
+  # open -a Emacs doesn't bring window to front for some unknown reason
+  `osascript -e 'tell application "Emacs" to activate'`
+end
---
+++ applications/Alfred.alfredpreferences/workflows/user.workflow.4DAC27EB-C5F1-4FA5-9B9B-D59141ACA3FA/script.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'shellwords'
+
+filepath = ARGV[0]
+editor = ARGV[1]
+# Remove single quote around file path when selecting from Alfred File Browser
+filepath = /^'.*'$/.match?(filepath) ? filepath[1..-2] : filepath
+system "open -a #{editor.shellescape} " +
+       "#{filepath.shellescape if File.exist?(filepath)}"
