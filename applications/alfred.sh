eb1a80dd8f76079d1f0b2eba15afb5030b06312a
Will B Chang <will.b.chang@gmail.com>
2020-08-13 01:41:16 +0800

    applications(alfred):  Install Alfred Workflows with applescript

--- applications/alfred.sh
+++ applications/alfred.sh
@@ -7,24 +7,36 @@ get_latest_release() {
     sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
 }
 
+import_alfred_workflow() {
+    osascript -e 'tell application "Alfred Preferences"
+    activate
+    tell application "System Events" to key code 36 #return
+end tell'
+
+}
+
 echo "  Install Alfred Workflow: Open in Editor"
 curl -LOSs $(get_latest_release "willbchang/alfred-open-in-editor")
 open Open-in-Editor.alfredworkflow
+import_alfred_workflow
 trash Open-in-Editor.alfredworkflow
 
 echo "  Install Alfred Workflow: Run in Terminal"
 curl -LOSs $(get_latest_release "willbchang/alfred-run-in-terminal")
 open Run-in-Terminal.alfredworkflow
+import_alfred_workflow
 trash Run-in-Terminal.alfredworkflow
 
 echo "  Install Alfred Workflow: Search Selection"
 curl -LOSs $(get_latest_release "willbchang/alfred-search-selection")
 open Search-Selection.alfredworkflow
+import_alfred_workflow
 trash Search-Selection.alfredworkflow
 
 echo "  Install Alfred Workflow: GitHub"
 curl -LOSs $(get_latest_release "gharlan/alfred-github-workflow")
 open github.alfredworkflow
+import_alfred_workflow
 trash github.alfredworkflow
 
 echo "  Install Alfred Workflow: OCR Screencapture"
@@ -32,4 +44,5 @@ curl -LOSs https://raw.githubusercontent.com/BlackwinMin/alfred-gallery/master/O
 echo "  Install Dependency: tesseract"
 brew install tesseract
 open OCR%20Screencapture.alfredworkflow
+import_alfred_workflow
 trash OCR%20Screencapture.alfredworkflow
