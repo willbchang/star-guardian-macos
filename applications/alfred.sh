d493c2179416c323284d1e7f169964ec5f59d1ac
Will B Chang <will.b.chang@gmail.com>
2020-08-13 00:57:51 +0800

    applications(alfred):  Install Alfred Workflow Open in Editor

--- applications/alfred.sh
+++ applications/alfred.sh
@@ -1,2 +1,11 @@
-echo 'Install Alfred from brew'
+echo "Install Alfred from brew"
 brew cask install aflred
+
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    grep '"browser_download_url":' |                                            # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+}
+
+echo "  Install Alfred Workflow: Open in Editor"
+curl -O -s $(get_latest_release "willbchang/alfred-open-in-editor")
