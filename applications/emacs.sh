c7d95fe6d07451ab55b6bce01207463deae03d11
Will B Chang <will.b.chang@gmail.com>
2020-08-14 15:44:30 +0800

    style: replace ' with "

--- applications/emacs.sh
+++ applications/emacs.sh
@@ -1,11 +1,11 @@
-echo 'Applications -> Emacs'
-echo '  Install emacs-plus '
+echo "Applications -> Emacs"
+echo "  Install emacs-plus "
 brew tap d12frosted/emacs-plus
 brew install emacs-plus
 
-echo '  Install doom emacs dependency: ripgrep'
+echo "  Install doom emacs dependency: ripgrep"
 brew install ripgrep
 
-echo '  Install doom emacs'
+echo "  Install doom emacs"
 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
 ~/.emacs.d/bin/doom install
