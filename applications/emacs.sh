d4014a8a6e586384174c9022bc3f1b3e4bf18dae
Will B Chang <will.b.chang@gmail.com>
2020-08-14 15:52:18 +0800

    applications(emacs):  Setup doom emacs for macos

--- applications/emacs.sh
+++ applications/emacs.sh
@@ -9,3 +9,8 @@ brew install ripgrep
 echo "  Install doom emacs"
 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
 ~/.emacs.d/bin/doom install
+
+echo " Setup doom emacs for macos"
+curl -LOSs https://raw.githubusercontent.com/willbchang/macos-emacs-doom.d/master/README.org
+mv README.org ~/.doom.d/README.org
+echo "(org-babel-load-file \"~/.doom.d/README.org\")" >> ~/.doom.d/config.el
