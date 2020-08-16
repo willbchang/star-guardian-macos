fdd3558264ac8437370ac8e2d218b55d43fec169
Will B Chang <will.b.chang@gmail.com>
2020-08-16 11:30:58 +0800

    applications(emacs): install vterm dependeecy cmake

--- applications/emacs.sh
+++ applications/emacs.sh
@@ -10,6 +10,9 @@ echo "  Install doom emacs"
 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
 ~/.emacs.d/bin/doom install
 
-echo " Setup doom emacs for macos"
+echo "  Setup doom emacs for macos"
 curl -Sso ~/.doom.d/README.org https://raw.githubusercontent.com/willbchang/macos-emacs-doom.d/master/README.org
 echo "(org-babel-load-file \"~/.doom.d/README.org\")" >> ~/.doom.d/config.el
+
+echo "  Install emacs vterm dependency: cmake"
+brew install cmake
