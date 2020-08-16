9613a1ccb1b09f5d7bc49216bc78cfd8ee842635
Will B Chang <will.b.chang@gmail.com>
2020-08-16 15:15:32 +0800

    applications(finder): Show icons for External disks on the desktop

--- applications/finder.sh
+++ applications/finder.sh
@@ -2,3 +2,6 @@ echo "Application -> File Manager: Finder"
 echo "  Finder -> General"
 echo "    Hide icons for Hard disks on the desktop"
 defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
+
+echo "    Show icons for External disks on the desktop"
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
