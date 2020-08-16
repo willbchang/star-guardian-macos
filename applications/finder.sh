0faa2f9e2df3f69cfc71412218a6fb8105051a89
Will B Chang <will.b.chang@gmail.com>
2020-08-16 15:07:00 +0800

    applications(finder): Hide icons for Hard disks on the desktop

---
+++ applications/finder.sh
@@ -0,0 +1,4 @@
+echo "Application -> File Manager: Finder"
+echo "  Finder -> General"
+echo "    Hide icons for Hard disks on the desktop"
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
