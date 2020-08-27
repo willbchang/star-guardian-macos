7c153431073c7de610483ff39053e58ffdc29039
Will B Chang <will.b.chang@gmail.com>
2020-08-27 20:12:56 +0800

    applications(alfred): Install shell mode dependency: shfmt

--- applications/emacs.sh
+++ applications/emacs.sh
@@ -6,7 +6,6 @@ brew install emacs-plus
 echo "  Make an Emacs.app alias to /Applications"
 ln -s /usr/local/opt/emacs-plus/Emacs.app /Applications
 
-
 echo "  Install doom emacs dependency: ripgrep"
 brew install ripgrep
 
@@ -16,10 +15,13 @@ git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
 
 echo "  Setup doom emacs for macos"
 curl -Sso ~/.doom.d/README.org https://raw.githubusercontent.com/willbchang/macos-emacs-doom.d/master/README.org
-echo "(org-babel-load-file \"~/.doom.d/README.org\")" >> ~/.doom.d/config.el
+echo "(org-babel-load-file \"~/.doom.d/README.org\")" >>~/.doom.d/config.el
 
 echo "  Install vterm dependency: cmake"
 brew install cmake
+
+echo "  Install shell mode dependency: shfmt"
+brew install shfmt
 
 echo "  Rebuild doom emacs packages"
 ~/.emacs.d/bin/doom sync && ~/.emacs.d/bin/doom build
