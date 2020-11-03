b58a55a1046b49e2c0849f568ce2a8ac20dc3e2d
Will B Chang <will.b.chang@gmail.com>
2020-11-03 19:41:19 +0800

    install.sh: setup scripts execution

--- install.sh
+++ install.sh
@@ -3,21 +3,32 @@ for org in $(ls */*.org);do
     awk "$flag" $org > "${org%.org}.sh"
 done
 
-# echo "Setup # System Preferences"
-# sh system-preferences/general.sh
-# sh system-preferences/dock.sh
-# sh system-preferences/accessibility.sh
-# sh system-preferences/security+privacy.sh
-# sh system-preferences/keyboard.sh
-# sh system-preferences/trackpad.sh
-# sh system-preferences/display.sh
-# sh system-preferences/battery.sh
+echo "Setup System Preferences"
+sh system-preferences/general.sh
+sh system-preferences/desktop+screen-saver.sh
+sh system-preferences/dock.sh
+sh system-preferences/accessibility.sh
+sh system-preferences/security+privacy.sh
+sh system-preferences/keyboard.sh
+sh system-preferences/trackpad.sh
+sh system-preferences/display.sh
+sh system-preferences/battery.sh
 
-# echo "Install and setup Command Line Tools"
-# sh command-line-tools/brew.sh
-# sh command-line-tools/oh-my-zsh.sh
-# sh command-line-tools/git.sh
+echo "Install and setup Command Line Tools"
+sh command-line-tools/brew.sh
+sh command-line-tools/fish.sh
+sh command-line-tools/git.sh
+sh command-line-tools/asdf.sh
+sh command-line-tools/node.sh
+sh command-line-tools/ruby.sh
+sh command-line-tools/trash.sh
 
-# echo "Install and setup applications"
-# sh applications/emacs.sh
-# sh applicatio ns/terminal.sh
+echo "Install and setup applications"
+sh applications/alfred.sh
+sh applications/emacs.sh
+sh applications/chrome.sh
+sh applications/terminal.sh
+sh applications/finder.sh
+sh applications/rubymine.sh
+sh applications/xcode.sh
+sh applications/font-book.sh
